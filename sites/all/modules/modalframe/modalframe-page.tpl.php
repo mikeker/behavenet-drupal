5e2c8e230710295d866590fbd217e18e8a82be9a
Mike Keran <info@mikekeran.com>
2014-10-16 09:34:00 +0200

    Updated modalframe -- security update.

--- sites/all/modules/modalframe/modalframe-page.tpl.php
+++ sites/all/modules/modalframe/modalframe-page.tpl.php
@@ -20,7 +20,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
 <head>
 <?php print $head; ?>
-<title><?php print (!empty($title) ? strip_tags($title) : $head_title); ?></title>
+<title><?php print (!empty($title) ? $title : $head_title); ?></title>
 <?php print $styles; ?>
 <?php print $scripts; ?>
 </head>
